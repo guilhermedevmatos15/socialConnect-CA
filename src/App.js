3526ff2c4a6c0b1780024d220b0b0cd6b2569cdc
guilhermedevmatos15 <devguilhermeferreira@gmail.com>
2023-09-07 15:16:03 -0300

    feat: Agora sim, AOS pronto para ser usado

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ import { Outlet } from 'react-router-dom';
 // ? Styles
 import './app/styles/index.scss';
 
+// ? AOS (animation)
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+AOS.init({
+   duration: 1300,
+});
+
 const App = () => {
    return (
       <>
